3add687773e0476bc91657f1561f33e8b93e79cc
xiaosan <xiaosan123.com>
2021-07-29 14:18:05 +0800

    电话本项目--创建了核心的业务类

--- src/com/feicailian/phonebook/Operate.java
+++ src/com/feicailian/phonebook/Operate.java
@@ -1,13 +1,167 @@
 package com.feicailian.phonebook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 核心业务类
  *  方法：
  *      private List<Person> list   容器
  *      addLogic ()         用户添加记录业务逻辑控制
  *      searchLogic ()      用户查询记录业务逻辑控制
+ *      modifyLogic ()      修改记录业务逻辑控制
+ *      deleteLogic ()      删除记录业务逻辑控制
+ *      orderLogic ()       排序记录业务逻辑控制
+ *      addOperation ()     添加新记录信息
+ *
+ *      showAll ()          查询全记录
+ *      searchByName ()     按姓名查询记录
+ *      searchByAge ()      按年龄查询记录
+ *      searchBySex ()      按性别查询记录
+ *      searchByTelNum ()   按电话查询记录
+ *      searchByAdd ()      按地址查询记录
+ *
+ *      modifyOperation ()  修改指定记录
+ *      deleteOperation ()  删除指定记录
+ *      deleteAllOperation ()  删除全部记录
+ *
+ *      orderName ()        按用户姓名排序记录
+ *      orderAge ()         按用户年龄排序记录
+ *      orderSex ()         按用户性别排序记录
+ *
  *
  */
 public class Operate {
+
+    private List <Person> list ;
+
+    public Operate () {
+        this.list = new ArrayList<>() ;
+    }
+
+    /**
+     *      addLogic ()         用户添加记录业务逻辑控制
+     */
+    public void addLogic(){
+
+    }
+
+    /**
+     *      searchLogic ()      用户查询记录业务逻辑控制
+     */
+    public void searchLogic(){
+
+    }
+
+    /**
+     *      modifyLogic ()      修改记录业务逻辑控制
+     */
+    public void modifyLogic(){
+
+    }
+    /**
+     *      deleteLogic ()      删除记录业务逻辑控制
+     */
+    public void deleteLogic(){
+
+    }
+
+    /**
+     *      orderLogic ()       排序记录业务逻辑控制
+     */
+    public void orderLogic() {
+
+    }
+
+    /**
+     *      addOperation ()     添加新记录信息
+      */
+    public void addOperation(){
+
+    }
+
+    /**
+     *      showAll ()          查询全记录
+     */
+    public void showAll(){
+
+    }
+
+    /**
+     *      searchByName ()     按姓名查询记录
+     */
+    public  void searchByName(){
+
+    }
+
+    /**
+     *      searchByAge ()      按年龄查询记录
+     */
+    public  void searchByAge(){
+
+    }
+
+        /**
+         *      searchBySex ()      按性别查询记录
+         */
+    public  void searchBySex(){
+
+    }
+
+    /**
+     *      searchByTelNum ()   按电话查询记录
+     */
+    public  void searchByTelNum(){
+
+    }
+
+    /**
+     *      searchByAdd ()      按地址查询记录
+     */
+    public  void searchByAdd(){
+
+    }
+
+    /**
+     *      modifyOperation ()  修改指定记录
+     */
+    public void modifyOperation(){
+
+    }
+
+    /**
+     *      deleteOperation ()  删除指定记录
+     */
+    public void deleteOperation(){
+
+    }
+
+    /**
+     *      deleteAllOperation ()  删除全部记录
+     */
+    public void deleteAllOperation(){
+
+    }
+
+    /**
+     *      orderName ()        按用户姓名排序记录
+     */
+    public void orderName(){
+
+    }
+
+    /**
+     *      orderAge ()         按用户年龄排序记录
+     */
+    public void orderAge(){
+
+    }
+
+    /**
+     *      orderSex ()         按用户性别排序记录
+     */
+    public void orderSex(){
+
+    }
 
 }
