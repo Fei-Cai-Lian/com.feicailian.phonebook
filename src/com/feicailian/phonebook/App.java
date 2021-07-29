b72b5ec3d7e9d3ba1d946760b91880af4d962a94
xiaosan <xiaosan123.com>
2021-07-29 12:58:44 +0800

    电话本项目的预缓存

---
+++ src/com/feicailian/phonebook/App.java
@@ -0,0 +1,21 @@
+package com.feicailian.phonebook;
+
+public class App {
+    /**
+     * 启动电话本项目
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 控制主菜单
+     */
+    public void start() {
+
+    }
+
+}
