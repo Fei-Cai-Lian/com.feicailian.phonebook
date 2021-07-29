4613e71c03a337baaba24c6350e0abbbec2b676e
xiaosan <xiaosan123.com>
2021-07-29 13:47:32 +0800

    电话本项目--添加了实体类-Person

---
+++ src/com/feicailian/phonebook/Operate.java
@@ -0,0 +1,13 @@
+package com.feicailian.phonebook;
+
+/**
+ * 核心业务类
+ *  方法：
+ *      private List<Person> list   容器
+ *      addLogic ()         用户添加记录业务逻辑控制
+ *      searchLogic ()      用户查询记录业务逻辑控制
+ *
+ */
+public class Operate {
+
+}
